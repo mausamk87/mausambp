aa62af433209e4821d9a7b514a68d4ccf74a3aa1
mausamk87 <comionagpur@gmail.com>
2026-04-05 23:21:19 +0530

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE='mausam-bp-v6';
-self.addEventListener('install',e=>{self.skipWaiting();});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(c=>c!==CACHE).map(c=>caches.delete(c)))));self.clients.claim();});
-self.addEventListener('fetch',e=>{if(e.request.url.includes('firebase')||e.request.url.includes('gstatic')||e.request.url.includes('googleapis')||e.request.url.includes('cdnjs'))return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
